b3e048191c69d33e3437b9eda97a0fa3a94fec60
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-09-28 20:57:55 -0500

    updated functions

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,4 @@
 // * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-
-// * `selectAll()`
-// * `insertOne()`
 // * `updateOne()`
 
 // * Export the ORM object in `module.exports`.
@@ -38,7 +35,7 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 var orm = {
-  all: function (tableInput, cb) {
+  selectAll: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function (err, result) {
       if (err) {
@@ -47,7 +44,7 @@ var orm = {
       cb(result);
     });
   },
-  create: function (table, cols, vals, cb) {
+  insertOne: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -68,7 +65,7 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  update: function (table, objColVals, condition, cb) {
+  updateOne: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
@@ -77,19 +74,6 @@ var orm = {
     queryString += condition;
 
     console.log(queryString);
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  delete: function (table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
     connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,8 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-  burger.all(function (data) {
+  burger.selectAll(function (data) {
+    //calls to burger.js to get the function for ORM and creates into JSON
     var hbsObject = {
       burgers: data,
     };
@@ -16,12 +17,14 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-  burger.create(["name", "devour"], [req.body.name, req.body.devour], function (
-    result
-  ) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+  burger.insertOne(
+    ["name", "devoured"],
+    [req.body.name, req.body.devour],
+    function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    }
+  );
 });
 
 router.put("/api/burgers/:id", function (req, res) {
@@ -29,7 +32,7 @@ router.put("/api/burgers/:id", function (req, res) {
 
   console.log("condition", condition);
 
-  burger.update(
+  burger.updateOne(
     {
       devour: req.body.devour,
     },
@@ -43,19 +46,6 @@ router.put("/api/burgers/:id", function (req, res) {
       }
     }
   );
-});
-
-router.delete("/api/burgers/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function (result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
 });
 
 // Export routes for server.js to use.
