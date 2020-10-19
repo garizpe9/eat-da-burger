09a7c399ba5e532958aa60d3762b42c8449205b9
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-10-19 15:52:44 -0500

    changing back to old orm

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-// Requiring necessary npm packages
-const express = require("express");
+var express = require("express");
 
-// Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
-const db = require("./models");
+var PORT = process.env.PORT || 3000;
 
-// Creating express app and configuring middleware needed for authentication
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Requiring our routes
- require("./routes/html-routes")(app);
- require("./routes/api-routes.js")(app);
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -21,13 +17,11 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
 });
