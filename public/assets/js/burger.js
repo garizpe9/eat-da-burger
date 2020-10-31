48cfdbb6f059ae1224108753f8598bef0bf5f8f8
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-10-31 16:05:04 -0500

    css7

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".dangergit").on("click", function(event) {
+  $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
     var borg = $(this).data("newburger");
 
